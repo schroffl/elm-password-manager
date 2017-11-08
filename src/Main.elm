8999e0e3be264b1f94a4a0afbc8227a1821f8086
schroffl <hannes.holzwarth@me.com>
2017-11-08 15:18:32 +0100

    Update src/Main.elm - main now returns an html program

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,36 @@
 module Main exposing (main)
 
 import Html exposing (..)
-import Crypto
+import Crypto exposing (CryptoKey, CryptoError)
+
+
+type Msg
+    = GotKey (Result CryptoError CryptoKey)
+
+
+type alias Model =
+    {}
 
 
 main =
+    program
+        { init = init
+        , update = update
+        , subscriptions = always Sub.none
+        , view = view
+        }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( {}, Cmd.none )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
     text "test"
