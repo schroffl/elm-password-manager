9c311bd79ab754a2528998d98204d793ef06a049
schroffl <schroffl@users.noreply.github.com>
2017-12-05 20:23:36 +0100

    Trying out elm-lang/navigation and rtfeldman/elm-css

--- src/Main.elm
+++ src/Main.elm
@@ -1,48 +1,166 @@
 module Main exposing (main)
 
-import Html exposing (..)
-import Html.Attributes exposing (style)
-import Crypto exposing (CryptoKey, CryptoError)
-import Setup
+import Html.Styled exposing (..)
+import Html.Styled.Attributes exposing (..)
+import Html.Styled.Events exposing (onInput, onClick)
+import Html.Styled.Keyed as Keyed
+import Html as StandardHtml
+import Navigation
+import UrlParser
+import Styles
 
 
 type Msg
-    = SetupMessage Setup.Msg
+    = LocationChange Navigation.Location
+    | Goto Route
+    | PasswordInput String
+
+
+type Route
+    = LoginRoute
+    | VaultRoute
+    | NotFoundRoute
+
+
+type alias Password =
+    String
 
 
 type alias Model =
-    { setupModel : Setup.Model }
+    { route : Route
+    , password : Password
+    }
 
 
+meta : String -> String -> Html Msg
+meta name_ content_ =
+    node "meta" [ name name_, content content_ ] []
+
+
+matchers : UrlParser.Parser (Route -> a) a
+matchers =
+    UrlParser.oneOf
+        [ UrlParser.map LoginRoute <| UrlParser.s "login"
+        , UrlParser.map VaultRoute <| UrlParser.top
+        ]
+
+
+parseLocation : Navigation.Location -> Route
+parseLocation location =
+    let
+        result =
+            UrlParser.parsePath matchers location
+    in
+        case result of
+            Just route ->
+                route
+
+            Nothing ->
+                NotFoundRoute
+
+
+goto : Route -> Cmd Msg
+goto route =
+    Navigation.newUrl <|
+        case route of
+            LoginRoute ->
+                "/login"
+
+            VaultRoute ->
+                "/"
+
+            NotFoundRoute ->
+                "/"
+
+
+main : Program Never Model Msg
 main =
-    program
+    Navigation.program LocationChange
         { init = init
-        , update = update
         , subscriptions = always Sub.none
+        , update = update
         , view = view
         }
 
 
-init : ( Model, Cmd Msg )
-init =
-    ( { setupModel = Setup.init }, Cmd.none )
+init : Navigation.Location -> ( Model, Cmd Msg )
+init location =
+    ( { route = parseLocation location
+      , password = ""
+      }
+    , Cmd.none
+    )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        SetupMessage setupMsg ->
-            let
-                ( newModel, command ) =
-                    Setup.update setupMsg model.setupModel
-            in
-                ( { model | setupModel = newModel }
-                , Cmd.map SetupMessage command
-                )
+        LocationChange location ->
+            ( { model | route = parseLocation location }
+            , Cmd.none
+            )
+
+        Goto route ->
+            ( model, goto route )
+
+        PasswordInput newPassword ->
+            ( { model | password = newPassword }, Cmd.none )
 
 
-view : Model -> Html Msg
+view : Model -> StandardHtml.Html Msg
 view model =
-    div [ style [ ( "height", "100%" ) ] ]
-        [ Html.map SetupMessage <| Setup.view model.setupModel
+    let
+        containerDiv =
+            toUnstyled << Keyed.node "div" [ css Styles.container ]
+    in
+        containerDiv <|
+            ( "viewport-meta", meta "viewport" "width=device-width, initial-scale=1, user-scalable=no" )
+                :: case model.route of
+                    LoginRoute ->
+                        [ ( "lock", lockView model.password False ) ]
+
+                    VaultRoute ->
+                        [ ( "lock", lockView model.password True )
+                        , ( "vault", vaultView )
+                        ]
+
+                    NotFoundRoute ->
+                        [ ( "not-found", text "404 Not Found" ) ]
+
+
+lockView : Password -> Bool -> Html Msg
+lockView password unlocked =
+    let
+        additionalStyles =
+            if unlocked then
+                Styles.lockUnlocked
+            else
+                []
+    in
+        div
+            [ css <| Styles.lock ++ additionalStyles ]
+            [ div [ css Styles.lockInputContainer ]
+                [ input
+                    [ type_ "password"
+                    , placeholder "Password"
+                    , onInput PasswordInput
+                    , value password
+                    , css Styles.lockInput
+                    ]
+                    []
+                , button [ onClick <| Goto VaultRoute ] [ text "login" ]
+                ]
+            ]
+
+
+vaultView : Html Msg
+vaultView =
+    div
+        [ onClick <| Goto LoginRoute
         ]
+        [ text "Vault Route" ]
+
+
+vaultHeader : Html Msg
+vaultHeader =
+    div [] []
---
+++ src/Styles.elm
@@ -0,0 +1,106 @@
+module Styles exposing (..)
+
+import Css exposing (..)
+import Css.Media as Media exposing (withMedia)
+
+
+type alias Theme =
+    { primary : Color
+    , secondary : Color
+    , highlightColor : Color
+    }
+
+
+defaultTheme : Theme
+defaultTheme =
+    { primary = hex "ccc"
+    , secondary = hsl 0 0 0.95
+    , highlightColor = hex "80bdff"
+    }
+
+
+smallWidth : Media.MediaQuery
+smallWidth =
+    Media.all [ Media.maxWidth (px 800) ]
+
+
+smallHeight : Media.MediaQuery
+smallHeight =
+    Media.all [ Media.maxHeight (px 500) ]
+
+
+container : List Style
+container =
+    [ position absolute
+    , overflowX hidden
+    , width (pct 40)
+    , marginLeft (pct 30)
+    , marginRight (pct 30)
+    , border3 (px 1) solid defaultTheme.primary
+    , borderRadius (px 3)
+    , top (px 20)
+    , bottom (px 20)
+    , withMedia [ smallWidth ]
+        [ width (pct 100)
+        , margin (px 0)
+        , top (px 0)
+        , bottom (px 0)
+        ]
+    ]
+
+
+lock : List Style
+lock =
+    [ position absolute
+    , backgroundColor defaultTheme.secondary
+    , width (pct 100)
+    , height (pct 100)
+    , backgroundImage (url "/img/gear.svg")
+    , backgroundRepeat noRepeat
+    , backgroundSize contain
+    , backgroundPosition center
+    , displayFlex
+    , alignItems center
+    , justifyContent center
+    , property "transition" "margin-left ease-out, 300ms"
+    ]
+
+
+lockUnlocked : List Style
+lockUnlocked =
+    [ marginLeft (pct -100)
+    ]
+
+
+lockInputContainer : List Style
+lockInputContainer =
+    [ position relative
+    , width (pct 50)
+    , margin (px 0)
+    , padding (px 0)
+    , displayFlex
+    , flexWrap wrap
+    , justifyContent center
+    , withMedia [ smallWidth ]
+        [ width (pct 60) ]
+    ]
+
+
+lockInput : List Style
+lockInput =
+    [ width (pct 100)
+    , height (px 35)
+    , fontSize (px 18)
+    , border3 (px 1) solid defaultTheme.primary
+    , borderRadius (px 3)
+    , textAlign center
+    , property "transition" "border-color, 100ms linear"
+    , focus
+        [ outline none
+        , borderColor defaultTheme.highlightColor
+        ]
+    , withMedia [ smallHeight ]
+        [ height (px 20)
+        , fontSize (px 15)
+        ]
+    ]
