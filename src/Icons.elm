ba97834d37d3a640f595652a31cc9c4f99661106
schroffl <hannes.holzwarth@me.com>
2017-12-06 23:45:57 +0100

    Add some feather-icons for later usage
    The original feathericons can be found on 'https://feathericons.com/'. For generating the svg elm code 'https://1602.github.io/elm-feather-icons/' was used.

---
+++ src/Icons.elm
@@ -0,0 +1,92 @@
+module Icons
+    exposing
+        ( check
+        , clipboard
+        , edit2
+        , lock
+        , search
+        , settings
+        , trash
+        , x
+        )
+
+import Html exposing (Html)
+import Svg exposing (Svg, svg)
+import Svg.Attributes exposing (..)
+
+
+svgFeatherIcon : String -> List (Svg msg) -> Html msg
+svgFeatherIcon className =
+    svg
+        [ class <| "feather feather-" ++ className
+        , fill "none"
+        , height "24"
+        , stroke "currentColor"
+        , strokeLinecap "round"
+        , strokeLinejoin "round"
+        , strokeWidth "2"
+        , viewBox "0 0 24 24"
+        , width "24"
+        ]
+
+
+check : Html msg
+check =
+    svgFeatherIcon "check"
+        [ Svg.polyline [ points "20 6 9 17 4 12" ] []
+        ]
+
+
+clipboard : Html msg
+clipboard =
+    svgFeatherIcon "clipboard"
+        [ Svg.path [ d "M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2" ] []
+        , Svg.rect [ Svg.Attributes.x "8", y "2", width "8", height "4", rx "1", ry "1" ] []
+        ]
+
+
+edit2 : Html msg
+edit2 =
+    svgFeatherIcon "edit-2"
+        [ Svg.polygon [ points "16 3 21 8 8 21 3 21 3 16 16 3" ] []
+        ]
+
+
+lock : Html msg
+lock =
+    svgFeatherIcon "lock"
+        [ Svg.rect [ Svg.Attributes.x "3", y "11", width "18", height "11", rx "2", ry "2" ] []
+        , Svg.path [ d "M7 11V7a5 5 0 0 1 10 0v4" ] []
+        ]
+
+
+search : Html msg
+search =
+    svgFeatherIcon "search"
+        [ Svg.circle [ cx "10.5", cy "10.5", r "7.5" ] []
+        , Svg.line [ x1 "21", y1 "21", x2 "15.8", y2 "15.8" ] []
+        ]
+
+
+settings : Html msg
+settings =
+    svgFeatherIcon "settings"
+        [ Svg.circle [ cx "12", cy "12", r "3" ] []
+        , Svg.path [ d "M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z" ] []
+        ]
+
+
+trash : Html msg
+trash =
+    svgFeatherIcon "trash"
+        [ Svg.polyline [ points "3 6 5 6 21 6" ] []
+        , Svg.path [ d "M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" ] []
+        ]
+
+
+x : Html msg
+x =
+    svgFeatherIcon "x"
+        [ Svg.line [ x1 "18", y1 "6", x2 "6", y2 "18" ] []
+        , Svg.line [ x1 "6", y1 "6", x2 "18", y2 "18" ] []
+        ]
